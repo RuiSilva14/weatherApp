a5b29ceb7461a8413e7328f44bbee6d7bfc9113c
Rui Silva <rui.silva@upeoconsulting.com>
2025-07-09 16:02:03 +0200

    Weather Email Notification based on the prefered cities

--- force-app/main/default/lwc/weatherPlaying/weatherPlaying.js
+++ force-app/main/default/lwc/weatherPlaying/weatherPlaying.js
@@ -1,36 +1,74 @@
-import { LightningElement, track, wire, api } from "lwc";
-import { getRecord } from "lightning/uiRecordApi";
-import getWeather from "@salesforce/apex/weatherPlaying.getWeather";
+import { LightningElement, api, wire } from "lwc";
+import getWeatherReports from "@salesforce/apex/weatherPlaying.getWeatherReports";
+import getUserCityPreferences from "@salesforce/apex/weatherPlaying.getUserCityPreferences";
 
-import BILLING_CITY from "@salesforce/schema/Account.BillingCity";
+import removeCity from "@salesforce/apex/weatherPlaying.removeCity";
+import createCityPreference from "@salesforce/apex/weatherPlaying.createCityPreference";
 
 export default class WeatherPlaying extends LightningElement {
   @api recordId;
-  city;
+  weatherReports;
+  cityPreferences;
+  newCity;
 
-  @track weatherData = {
-    temperature: 0,
-    conditionText: "",
-    icon: ""
-  };
+  columns = [
+    { label: "City", fieldName: "City__c", type: "text" },
+    {
+      label: "Temperature",
+      fieldName: "Temperature__c",
+      type: "number"
+    },
+    {
+      label: "Weather Condition",
+      fieldName: "Weather_Condition__c",
+      type: "text"
+    },
+    { label: "Date", fieldName: "Date__c", type: "date" }
+  ];
 
-  @wire(getRecord, {
-    recordId: "$recordId",
-    fields: [BILLING_CITY]
-  })
-  WiredAccount({ error, data }) {
+  @wire(getUserCityPreferences)
+  cityPreferencesList({ error, data }) {
     if (data) {
-      this.city = data.fields.BillingCity.value;
-
-      getWeather({ city: this.city })
-        .then((result) => {
-          this.weatherData = result;
-        })
-        .catch((err) => {
-          console.log("This is the error", err);
-        });
+      this.cityPreferences = data;
     } else if (error) {
       console.log("This is the error", error);
     }
+  }
+
+  @wire(getWeatherReports, { accountId: "$recordId" })
+  weatherReportsList({ error, data }) {
+    if (data) {
+      this.weatherReports = data;
+    } else if (error) {
+      console.log("This is the error", error);
+    }
+  }
+
+  removeCityPreference(event) {
+    const id = event.target.dataset.id;
+    removeCity({ cityPreferenceId: id })
+      .then(() => {
+        this.cityPreferences = this.cityPreferences.filter(
+          (city) => city.Id !== id
+        );
+      })
+      .catch((error) => {
+        console.log("This is the error", error);
+      });
+  }
+
+  handleCityChange(event) {
+    this.newCity = event.target.value;
+  }
+
+  addCityPreference() {
+    createCityPreference({ city: this.newCity })
+      .then((data) => {
+        this.cityPreferences = [...this.cityPreferences, data];
+        this.newCity = "";
+      })
+      .catch((error) => {
+        console.log("This is the error", error);
+      });
   }
 }
